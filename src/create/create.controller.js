7ef5c58328c2fb45af38f6ad93e10394694e58dc
Claire <claire@agencetaurine.fr>
2017-12-07 15:15:35 +0100

    amelioration form create

--- src/create/create.controller.js
+++ src/create/create.controller.js
@@ -3,6 +3,33 @@ function CreateCtrl(){
 	const create = this;
 
 	create.message = "Hello create !";
+
+	// objet à insérer dans array de projets
+	create.newProject = {
+		title: "Mon titre",
+		client: "Cartoon Network",
+		authors: [],
+		brief: "Lorem ipsum dolor sit amet consectetur adisciping elit !"
+	}
+
+	create.chkBoxAuthors = {
+		jerome: false,
+		katell: false,
+		sabrina: false,
+		pascal: false,
+		laetitia: false,
+		claire: false,
+		matthias: false,
+		victor: false
+	}
+
+
+	// puis push dans create.newProject
+
+	create.save = function(){
+		console.log('Enregistré !');
+	}
+
 }
 
 export default CreateCtrl
